8f85a0b8fb8a733465dd9a99a15aa21177a0f066
Akdani Apriansyah <danniiapriansyah09@gmail.com>
2025-10-26 19:47:36 +0700

    Fix: Menggunakan DATABASE_URL dan mengoreksi pgsql config

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Artisan;
 
 Route::get('/run-migrate', function () {
     Artisan::call('migrate', ['--force' => true]);
-    return '✅ Migration berhasil dijalankan!';
+    return "✅ Migration PostgreSQL Render sukses!";
 });
