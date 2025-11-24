d3d02159b8ee219f1ad58510bc8ac3d9f8d28006
Akdani Apriansyah <danniiapriansyah09@gmail.com>
2025-11-24 19:55:13 +0700

    Add admins create tabel users

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,27 @@ Route::middleware(['web', 'admin'])->group(function () {
     Route::get('/admin/furniture', function () {
         return view('admin.furniture.index');
     })->name('admin.furniture');
-    // tabel users
-    Route::get('/admin/users', function () {
-        return view('admin.users.index');
-    })->name('admin.users');
-    Route::get('/admin/users/list', [UserController::class,'index']); // ajax data
-    Route::get('/admin/users/{id}', [UserController::class,'show']); // detail
-    Route::post('/admin/users/{id}', [UserController::class,'update']); // _method=PUT
-    Route::delete('/admin/users/{id}', [UserController::class,'destroy']);
+    // // tabel users
+    Route::prefix('/admin/users')->name('admin.users.')->group(function () {
+        
+        // Rute: /admin/users (menampilkan halaman)
+        Route::get('/', function () {
+            return view('admin.users.index');
+        })->name('index'); // nama rute: admin.users.index
+        
+        // Rute: /admin/users/list (ajax data)
+        Route::get('/list', [UserController::class, 'index']);
+        
+        // Rute: /admin/users/{id} (detail)
+        Route::get('/{id}', [UserController::class, 'show']); 
+        
+        // Rute: POST /admin/users/{id} (untuk Update dengan _method=PUT)
+        Route::post('/{id}', [UserController::class, 'update']);
+        
+        // Rute: DELETE /admin/users/{id} (Delete)
+        Route::delete('/{id}', [UserController::class, 'destroy']);
+        
+    });
 
     // Logout Admin
     Route::post('/admin/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
